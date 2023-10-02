b0025fb2e69c2eb7a3c5ca8f26345ef697a0f062
jsbol <jsbolaris@gmail.com>
2023-10-01 22:38:54 -0700

    second Commit

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,25 +1,9 @@
 // src/MyApp.js
 import React, {useState} from 'react';
 import Table from "./Table";
+import Form from "./Form";
 function MyApp() {
-	const [characters, setCharacters] = useState([
-	    {
-	        name: 'Charlie',
-	        job: 'Janitor',
-	    },
-      {
-        name: "Mac",
-        job: "Bouncer",
-      },
-      {
-        name: "Dee",
-        job: "Aspring actress",
-      },
-      {
-        name: "Dennis",
-        job: "Bartender",
-      },
-	]);  
+	const [characters, setCharacters] = useState([]);  
   function removeOneCharacter (index) {
     const updated = characters.filter((character, i) => {
         return i !== index
@@ -29,8 +13,16 @@ function MyApp() {
   return (
     <div className="container">
         <Table characterData={characters} removeCharacter={removeOneCharacter} />
+        <Form handleSubmit={updateList}/>
     </div>  
 )
+function updateList(person) {
+  setCharacters([...characters, person]);
+  <Form handleSubmit={updateList} />
+  
 }
+}
+
+
 
 export default MyApp;
--- src/Table.js
+++ src/Table.js
@@ -7,6 +7,7 @@ function TableHeader() {
           <th>Name</th>
           <th>Job</th>
         </tr>
+        
       </thead>
     );
   }
